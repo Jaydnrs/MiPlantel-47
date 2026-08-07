44e95cb3c3f79ba62458235793220420828af8b8
jayko <ivanspidey11@gmail.com>
2026-08-06 21:48:34 -0600

    primer prototipo

---
+++ main/java/mx/cbtis47/miplantel/Miplantel47Application.java
@@ -0,0 +1,13 @@
+package mx.cbtis47.miplantel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Miplantel47Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Miplantel47Application.class, args);
+	}
+
+}
---
+++ test/java/mx/cbtis47/miplantel/Miplantel47ApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.cbtis47.miplantel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Miplantel47ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
